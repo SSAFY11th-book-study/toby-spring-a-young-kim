bda20500e9c5de6c068a98fad43e6bc645991c7c
ayoung <ayoung7777@gmail.com>
2024-02-27 20:53:25 +0900

    2.5.2

--- src/main/java/springbook/user/dao/JUnitTest.java
+++ src/main/java/springbook/user/dao/JUnitTest.java
@@ -1,29 +1,53 @@
 package springbook.user.dao;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.hamcrest.CoreMatchers.sameInstance;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {DaoFactory.class})
 public class JUnitTest {
-    static JUnitTest testObject;
+    @Autowired
+    ApplicationContext context;
+    static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
-    public void test1(){
-        assertThat(this, is(not(sameInstance(testObject))));
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
-    public void test2(){
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
-    public void test3(){
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertThat(contextObject, either(is(nullValue())).or(is(this.contextObject)));
+        contextObject = this.context;
     }
 }
